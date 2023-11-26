4128f16f81fb2448b60d439733331ab5c627e05c
Akas_Datta <012akasdatta@gmail.com>
2023-11-27 04:54:50 +0600

    errorpage banner done

--- src/pages/Home/ErrorPage/Errorpage.jsx
+++ src/pages/Home/ErrorPage/Errorpage.jsx
@@ -1,17 +1,25 @@
-import { Link } from 'react-router-dom';
 import errorimage from '../../../../src/assets/404-banner.png'
 
 const ErrorPage = () => {
   return (
-    <div className='text-center error bg-[#E2E5E6] min-h-screen flex flex-col justify-center items-center text-black'>
-      <img className='errorImg w-48 h-auto sm:w-64 sm:h-auto mb-5' src={errorimage} alt="" />
-      <h2 className='text-2xl sm:text-lg md:text-2xl font-semibold mb-2'>Something went wrong.</h2>
-      <p className='text-sm sm:text-md md:text-lg lg:text-xl'>
-        We're sorry, but the page you're looking for could not be found.
-      </p>
-      <p>
-        Let's get you <Link to='/' className='text-blue-700'><u>back</u></Link>
-      </p>
+    <div>
+      <div className="relative h-screen overflow-hidden">
+        <div
+          className="bg-cover bg-center xl:h-96 lg:h-96 md:h-96 w-full h-72 flex items-center justify-center xl:mt-20 lg:mt-20 md:mt-0 mt-0"
+          style={{
+            backgroundImage: `url(${errorimage})`,
+          }}
+        >
+          <div className="text-[#231F20] font-medium first-letter:z-10 flex flex-col items-center justify-center pb-20 pt-14">
+            <span className='text-center banner-text py-10'>
+              <h2 className='xl:text-5xl lg:text-5xl md:text-5xl text-5xl font-serif'>Page not found.</h2>
+            </span>
+            <button className='text-xl text-white bg-[#718498] hover:bg-[#415239] px-6 py-3 font-semibold'>
+              Go Home
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,7 +3,6 @@ import ErrorPage from "../ErrorPage/Errorpage";
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
             <ErrorPage></ErrorPage>
         </div>
     );
