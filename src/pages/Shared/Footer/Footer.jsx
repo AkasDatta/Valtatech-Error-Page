d5fceebf558311e6048d41dfd5cb02f8d7151355
Akas_Datta <012akasdatta@gmail.com>
2023-11-27 22:41:41 +0600

    footer done

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,21 +1,24 @@
 
 const Footer = () => {
   return (
-    <div className='bg-white'>
+    <div className='bg-white py-8'>
     <div className="p-3 md:flex md:items-center md:justify-between max-w-screen-xl mx-auto lg:px-10 py-10 overflow-x-hidden">
-      <div className="flex justify-between items-center">
-        <div className="text-3xl text-white bg-[#31334E] px-5 py-3 rounded font-semibold cursor-default">
+      <div className="">
+        <div className="text-3xl text-white bg-[#31334E] px-5 py-3 rounded font-semibold cursor-pointer mb-4">
           <p>valtatech</p>
         </div>
-
+        <div>
+            <p className="text-lg">123 Waverly Pl., Boston, MA 02135</p>
+        </div>
       </div>
+
       <ul
         className="md:flex md:items-center md:justify-end z-[+1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-2 md:opacity-100 "
       >
-        <li className="m-2 text-sm">
-          <button className="text-xl text-white bg-[#718498] hover:bg-[#415239] px-6 py-3 font-semibold">
-            Contact us
-          </button>
+        <li className="m-2">
+          <div className="text-lg mb-20">
+          Copyright © 2023, Valtatech
+          </div>
         </li>
       </ul>
     </div>
