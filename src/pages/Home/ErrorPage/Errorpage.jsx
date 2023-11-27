8b7cc180c9e2fcb1caa0dc547d759b854ea1f7b9
Akas_Datta <012akasdatta@gmail.com>
2023-11-27 20:51:49 +0600

    react icon added

--- src/pages/Home/ErrorPage/Errorpage.jsx
+++ src/pages/Home/ErrorPage/Errorpage.jsx
@@ -3,14 +3,14 @@ import errorimage from '../../../../src/assets/404-banner.png'
 const ErrorPage = () => {
   return (
     <div>
-      <div className="relative h-screen overflow-hidden">
+      <div className="relative overflow-hidden">
         <div
           className="bg-cover bg-center xl:h-96 lg:h-96 md:h-96 w-full h-72 flex items-center justify-center xl:mt-20 lg:mt-20 md:mt-0 mt-0"
           style={{
             backgroundImage: `url(${errorimage})`,
           }}
         >
-          <div className="text-[#231F20] font-medium first-letter:z-10 flex flex-col items-center justify-center pb-20 pt-14">
+          <div className="text-[#231F20] font-medium first-letter:z-10 flex flex-col items-center justify-center pb-0 pt-14">
             <span className='text-center banner-text py-10'>
               <h2 className='xl:text-5xl lg:text-5xl md:text-5xl text-5xl font-serif'>Page not found.</h2>
             </span>
--- src/pages/Home/Home/MenuItem/Menuitem.jsx
+++ src/pages/Home/Home/MenuItem/Menuitem.jsx
@@ -1,19 +1,19 @@
-import { FaFacebookF, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaInstagram, FaLinkedinIn , FaTwitter } from "react-icons/fa";
 
 const MenuItem = () => {
   return (
     <div>
-      <div className="flex text-white mx-5 items-center justify-center">
-        <div className="hover:bg-teal-400 cursor-pointer bg-[#936565] rounded-full p-4">
+      <div className="flex text-white mx-5 mt-44 items-center justify-center">
+        <div className="hover:bg-[#230000] cursor-pointer bg-[#936565] rounded-full p-4">
             <FaFacebookF />
         </div>
-        <div className="hover:bg-teal-400 cursor-pointer bg-[#936565] rounded-full p-4 mx-3">
-            <FaLinkedin />
+        <div className="hover:bg-[#230000] cursor-pointer bg-[#936565] rounded-full p-4 mx-3">
+            <FaLinkedinIn />
         </div>
-        <div className="hover:bg-teal-400 cursor-pointer bg-[#936565] rounded-full p-4">
+        <div className="hover:bg-[#230000] cursor-pointer bg-[#936565] rounded-full p-4">
             <FaTwitter />
         </div>
-        <div className="hover:bg-teal-400 cursor-pointer bg-[#936565] rounded-full p-4 mx-3">
+        <div className="hover:bg-[#230000] cursor-pointer bg-[#936565] rounded-full p-4 mx-3">
             <FaInstagram />
         </div>
       </div>
